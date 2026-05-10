863fa46209c8a64fb66d8d1f74c1bc372852879c
Hussein Elmalek <egyptionpharoh5@gmail.com>
2026-05-10 19:13:57 +0400

    تفعيل نافذة تسجيل الدخول الذكية بدون حظر

--- models/models/Founder.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const founderSchema = new mongoose.Schema({
-    name: String,
-    bio: String,
-    imageUrl: String,
-    title: String
-});
-module.exports = mongoose.model('Founder', founderSchema);
